8ea4b771e4208bc08816f0e00e9eb9f4cd0911ec
Hoang Minh Thanh <sieusaochemgio@gmail.com>
2019-03-29 08:08:43 +0700

    Hoang Minh Thanh

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      Hoang Minh Thanh
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            lsdflsdfjklsdfj
           </p>
           <a
             className="App-link"
